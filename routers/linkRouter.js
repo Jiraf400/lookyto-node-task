30a14cf6dbf5880a2931308b5d4074008e68943d
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-10-24 18:43:19 +0300

    add route to getList method

--- routers/linkRouter.js
+++ routers/linkRouter.js
@@ -3,6 +3,7 @@ const router = new Router;
 const controller = require('../controllers/linkController');
 
 router.post('/create', controller.createLink);
+router.get('/getList', controller.getLinks);
 router.get('/**', controller.useShorterLink);
 
 module.exports = router;

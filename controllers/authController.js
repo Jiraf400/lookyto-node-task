3fbdbcfe8edace8e9057a087f8587a99bca2f7fb
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-10-24 19:45:02 +0300

    remove unnecessary quotes, elements, move generateAT method to bottom

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,30 +3,23 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const generateAccessToken = (id, username) => {
-    const payload = {
-        id, username
-    };
-    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '12h'})
-}
-
 class authController {
 
     async registration(req, res) {
-        const {username, password} = req.body;
-
-        if (!username || !password) return res.status(400).json({'message': 'Username and password are required.'});
-
-        const duplicate = await User.findOne({username: username}).exec();
-
-        if (duplicate) return res.sendStatus(409).json({'message': 'User already exists.'});
-
         try {
+            const {username, password} = req.body;
+
+            if (!username || !password) return res.status(400).json({'message': 'Username and password are required.'});
+
+            const duplicate = await User.findOne({username});
+
+            if (duplicate) return res.sendStatus(409).json({'message': 'User already exists.'});
+
             const hashedPwd = await bcrypt.hash(password, 10);
 
             const result = await User.create({
-                "username": username,
-                "password": hashedPwd
+                'username': username,
+                'password': hashedPwd
             });
 
             console.log(result);
@@ -41,14 +34,14 @@ class authController {
         const {username, password} = req.body;
         if (!username || !password) return res.status(400).json({'error': 'Username and password are required.'});
 
-        const foundUser = await User.findOne({username: username}).exec();
+        const foundUser = await User.findOne({username});
 
         if (!foundUser) return res.sendStatus(403).json({'error': `User not exists with username ${username}`});
 
         const match = await bcrypt.compare(password, foundUser.password);
         if (match) {
 
-            const accessToken = generateAccessToken(foundUser.id, foundUser.username)
+            const accessToken = generateAccessToken(foundUser._id, foundUser.username)
 
             const result = await foundUser.save();
             console.log(result);
@@ -59,6 +52,13 @@ class authController {
         }
     }
 
+}
+
+const generateAccessToken = (id, username) => {
+    const payload = {
+        id, username
+    };
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '12h'})
 }
 
 module.exports = new authController();
